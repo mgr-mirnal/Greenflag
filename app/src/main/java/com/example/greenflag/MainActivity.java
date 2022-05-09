25167b74bb6d1379da5f026fdd18dd6296f90e6b
Mirnal <mirnal@Mirnals-MacBook-Pro.local>
2022-05-09 01:13:47 +0100

    added new login

--- app/src/main/java/com/example/greenflag/MainActivity.java
+++ app/src/main/java/com/example/greenflag/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.greenflag;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnCreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    btnCreate = findViewById(R.id.btn_create_account);
+
+    btnCreate.setOnClickListener(view ->{
+        Intent intent = new Intent();
+        intent.setClass(this, login_page.class);
+        startActivity(intent);
+    });
     }
 }
